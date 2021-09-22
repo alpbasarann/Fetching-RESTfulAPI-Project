337e01832f5acdc027cafb31fd0119b0d1853f75
alpbasarann <alpbasaran99@gmail.com>
2021-09-22 17:48:48 +0300

    final code

--- src/components/UserCounter/index.js
+++ src/components/UserCounter/index.js
@@ -3,25 +3,15 @@ import { useSelector } from 'react-redux';
 import {fetchClosedPRs} from '../../store/actions';
 import { useDispatch } from 'react-redux';
 import {Area, Name} from './styled';
-import MergeFetching from '../../store';
-
-// in test
-// import the utility function X
-// mock function X and put some dummy mock return
 
 function UserCounter() {
   const [userRankList, setUserRankList] = useState([]);
+  const dispatch = useDispatch();
   const mergersArray = useSelector(state => state.mergers);
-  MergeFetching()
+
 
   useEffect(() => {
-    // in this file
-    // if store has state.mergers filled, do not fetch again
-    // you can put a flag to the store like; mergeFetching, mergeFetched
-    // if mergeFetched, do not call this
-    // if (!store.getState().mergers.mergeFetched) {
-    // }
-    // in this file, TODO move to utils, and add single line of code here to call the utility function X
+    dispatch(fetchClosedPRs())
     mergersArray.map((mergers) => {
       let indexOf = userRankList.findIndex((x) => Object.keys(x)[0] === mergers.user.login);
       if (indexOf !== -1) {
